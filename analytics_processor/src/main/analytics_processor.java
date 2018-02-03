94cb7ade9509cb63cf3df90d94adc2a73b6f14fe
mas-dse-pjahagir <pjahagir@eng.ucsd.edu>
2018-02-03 01:32:32 -0800

    Standford corenlp
    basic usage code

---
+++ analytics_processor/src/main/analytics_processor.java
@@ -0,0 +1,42 @@
+package main;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import static edu.stanford.nlp.util.Characters.isPunctuation;
+
+public class analytics_processor {
+
+
+    public static void main(String args[]) {
+        Properties props = new Properties();
+        props.put("annotators", "tokenize, ssplit, pos, lemma, ner");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        String text = "Barack Obama delivered a speech";
+
+        Annotation document = new Annotation(text);
+        pipeline.annotate(document);
+        List<String> results = new ArrayList<>();
+
+        List<CoreLabel> tokens = document.get(CoreAnnotations.TokensAnnotation.class);
+        for (CoreLabel tokensInfo : tokens) {
+            String token = tokensInfo.get(CoreAnnotations.TextAnnotation.class);
+            String lemma = tokensInfo.get(CoreAnnotations.LemmaAnnotation.class);
+            String pos = tokensInfo.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+            String ner = tokensInfo.get(CoreAnnotations.NamedEntityTagAnnotation.class);
+
+            results.add("Token: " + token + "; Lemma: " + lemma + "; POS: "+ pos + "; NER:" + ner + "\n");
+        }
+
+        for(String result:results) {
+            System.out.println(result);
+        }
+    }
+}
