2b2457484c5d3f210a5f3e93523baa84e96bd068
mas-dse-pjahagir <pjahagir@eng.ucsd.edu>
2018-02-14 22:47:55 -0800

    Extract verblist on sample data
    extract verblist

--- analytics_processor/src/verbExtractor/verbExtractor.java
+++ analytics_processor/src/verbExtractor/verbExtractor.java
@@ -13,11 +13,16 @@ import org.json.simple.parser.JSONParser;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class verbExtractor {
     StanfordCoreNLP pipeline;
-    String text;
+    ArrayList<String> newsArticles;
+    JSONArray verbList = new JSONArray();
+    String inFileName = "data/json/articles/caliNewsPaper_senate_sample.json";
+    String outFileName = "data/json/articles/caliNewsPaper_verbList.json";
 
     public verbExtractor()
     {
@@ -25,54 +30,73 @@ public class verbExtractor {
         props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse");
         this.pipeline = new StanfordCoreNLP(props);
 
-        this.text = this.loadJSON("data/json/articles/caliNewsPaper_senate_sample.json");
+        this.newsArticles = new ArrayList<>();
+
+        this.loadJSON(inFileName);
 
     }
 
     public void extractVerb()
     {
-        Annotation document = new Annotation(this.text);
-        pipeline.annotate(document);
+        for (String text: newsArticles) {
+//            Annotation document = new Annotation(String.join(" ", newsArticles));
+            Annotation document = new Annotation(text);
+            pipeline.annotate(document);
 
-        // these are all the sentences in this document
-        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+            // these are all the sentences in this document
+            // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+            List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
 
-        for (CoreMap sentence : sentences) {
-            // this is the Stanford dependency graph of the current sentence
-            SemanticGraph dependencies = sentence.get(SemanticGraphCoreAnnotations.EnhancedPlusPlusDependenciesAnnotation.class);
-            System.out.println(dependencies.toString());
+            for (CoreMap sentence : sentences) {
+                // this is the Stanford dependency graph of the current sentence
+                SemanticGraph dependencies = sentence.get(SemanticGraphCoreAnnotations.EnhancedPlusPlusDependenciesAnnotation.class);
+//                System.out.println(dependencies.toString());
 
-            IndexedWord rootVerb = dependencies.getFirstRoot();
-            System.out.println("rootverb :" + rootVerb.originalText() + " lemma: " + rootVerb.lemma());
+                IndexedWord rootVerb = dependencies.getFirstRoot();
+                if (rootVerb.ner().equals("O")) {
+                    System.out.println("rootverb :" + rootVerb.originalText() + " lemma: " + rootVerb.lemma());
+
+                    JSONObject obj = new JSONObject();
+                    obj.put("verb", rootVerb.originalText());
+                    obj.put("lemma", rootVerb.lemma());
+
+                    verbList.add(obj);
+                }
+            }
         }
 
+        try {
+
+            FileWriter file = new FileWriter(outFileName);
+            file.write(verbList.toJSONString());
+            file.flush();
+            file.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private String loadJSON(String filename) {
+    private void loadJSON(String filename) {
         JSONParser parser = new JSONParser();
-        StringBuilder sb = new StringBuilder();
 
         try {
             FileReader fileReader = new FileReader(filename);
             JSONArray jsonArray = (JSONArray) parser.parse(fileReader);
 
             Iterator i = jsonArray.iterator();
+
             while (i.hasNext())
             {
                 JSONObject jsonObject = (JSONObject)i.next();
                 String news = (String) jsonObject.get("news");
 
-                sb.append(news);
+                this.newsArticles.add(news);
             }
-
-            return sb.toString();
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
-        return null;
    }
 
     public static void main(String args[]) {
