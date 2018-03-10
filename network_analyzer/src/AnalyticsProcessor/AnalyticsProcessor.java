46190639575462c0f35a16f4395b50d5e607eab8
Jagirdar, Pavan <pavanj@qti.qualcomm.com>
2018-03-10 10:22:47 -0800

    capture Date & time information
    capture Date & time information
    cleanup unecessary code

--- network_analyzer/src/AnalyticsProcessor/AnalyticsProcessor.java
+++ network_analyzer/src/AnalyticsProcessor/AnalyticsProcessor.java
@@ -34,20 +34,21 @@ import utils.replace_UTF8;
 public class AnalyticsProcessor implements AutoCloseable {
     StanfordCoreNLP pipeline;
     ArrayList<NewsArticles> newsArticles;
-    JSONArray jsonGraphArray = new JSONArray();
+
     String inFileName = "data/json/graphbuilder/test2graph.json";
-    String outFileName = "data/json/graphbuilder/test2graph_output.json";
+
     private final Driver neo4jDriver;
 
     private class NewsArticles   {
         String newsArticle;
         String newsID;
+        String collectionDate;
 
-        NewsArticles (String news, Long id) {
+        NewsArticles (String news, Long id, String date) {
             newsArticle = news;
             newsID = id.toString();
+            collectionDate = date;
         }
-
     }
 
     public AnalyticsProcessor()
@@ -63,146 +64,8 @@ public class AnalyticsProcessor implements AutoCloseable {
         this.neo4jDriver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "1234"));
 
         try (Session session = neo4jDriver.session()) {
-            session.run("MATCH ()-[r]-() DELETE r");
-            session.run("MATCH (n) DELETE n");
+            session.run("MATCH (n) DETACH DELETE n");
         }
-
-
-    }
-
-    private Set<IndexedWord> findNmodChildren(SemanticGraph dependencies, IndexedWord rootVerb)
-    {
-        Set<IndexedWord> nmodChilds = new HashSet<>();
-
-        for (SemanticGraphEdge edge : dependencies.outgoingEdgeIterable(rootVerb)) {
-            if (edge.getRelation().toString().contains("nmod")) {
-                nmodChilds.add(edge.getTarget());
-            }
-        }
-
-        return nmodChilds;
-    }
-
-    private String getNer(String compound_string) {
-
-        String ner_label = null;
-
-        // Extract NER of this string
-        Annotation doc = new Annotation(compound_string);
-        pipeline.annotate(doc);
-
-        // these are all the sentences in this document
-        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-        List<CoreMap> sentences = doc.get(CoreAnnotations.SentencesAnnotation.class);
-
-        for (CoreMap sentence : sentences) {
-            // traversing the words in the current sentence
-            // a CoreLabel is a CoreMap with additional token-specific methods
-            List<CoreLabel> tokens = sentence.get(CoreAnnotations.TokensAnnotation.class);
-
-            for (CoreLabel tokensInfo : tokens) {
-                // this is the NER label of the token
-                ner_label = tokensInfo.get(CoreAnnotations.NamedEntityTagAnnotation.class);
-            }
-        }
-
-        return ner_label;
-    }
-
-    private String getAppos(SemanticGraph dependencies, IndexedWord tmp_word) {
-
-        ArrayList<String> appos = new ArrayList<>();
-
-        Set<IndexedWord> apposQualifiers = dependencies.getChildrenWithReln(tmp_word,
-                UniversalEnglishGrammaticalRelations.APPOSITIONAL_MODIFIER);
-
-        if(!apposQualifiers.isEmpty())   {
-
-            for(IndexedWord apposQualifier:apposQualifiers)   {
-                appos.add(apposQualifier.originalText());
-
-                Set<IndexedWord> dependents = dependencies.getChildren(apposQualifier);
-                for(IndexedWord dependent:dependents)   {
-                    appos.add(dependent.originalText());
-                }
-            }
-        }
-
-        return appos.toString();
-    }
-    private JSONObject getAdditionalNodeInfo(SemanticGraph dependencies, IndexedWord tmp_word) {
-        JSONObject jsonObj = new JSONObject();
-
-        jsonObj.put("NERLabel", tmp_word.ner());
-        jsonObj.put("Qualifier", getAppos(dependencies,tmp_word));
-
-        return jsonObj;
-    }
-
-    private JSONArray compoundStrings (SemanticGraph dependencies, Set<IndexedWord> tmp_words) {
-        JSONArray compoundJsonArray = new JSONArray();
-
-        ArrayList<String> compoundStringList = new ArrayList<>();
-
-        for (IndexedWord tmp_word : tmp_words) {
-            JSONObject compoundJsonObj = new JSONObject();
-            Set<IndexedWord> compound_nsubjs = dependencies.getChildrenWithReln(tmp_word,
-                    UniversalEnglishGrammaticalRelations.COMPOUND_MODIFIER);
-            String compound_string = tmp_word.originalText();
-
-            for (IndexedWord compound_nsubj : compound_nsubjs) {
-                compound_string = String.join(" ", compound_nsubj.originalText(), compound_string);
-            }
-            compoundStringList.add(compound_string);
-
-            compoundJsonObj.put("NodeID", compound_string);
-            // append addtional information
-            compoundJsonObj.put("MetaInfo",getAdditionalNodeInfo(dependencies, tmp_word));
-
-            compoundJsonArray.add(compoundJsonObj);
-        }
-
-        return compoundJsonArray;
-    }
-    private void createNode(String node, String compound_string) {
-        JSONObject jsonobj = new JSONObject();
-
-        System.out.println("Node: " + compound_string);
-    }
-
-    private JSONObject buildDependency(SemanticGraph dependencies) {
-        JSONObject jsonGraphObj = new JSONObject();
-        IndexedWord rootVerb = dependencies.getFirstRoot();
-
-        Set<IndexedWord> nsubjs = dependencies.getChildrenWithReln(rootVerb,
-                UniversalEnglishGrammaticalRelations.NOMINAL_SUBJECT);
-
-        if (!nsubjs.isEmpty()) {
-            JSONArray compoundJsonArr = compoundStrings(dependencies, nsubjs);
-            jsonGraphObj.put("SourceNode", compoundJsonArr);
-        }
-        else {
-
-        }
-
-        jsonGraphObj.put("Relation: ", rootVerb.lemma());
-
-        Set<IndexedWord> nmodChilds = findNmodChildren(dependencies,rootVerb);
-        if (!nmodChilds.isEmpty())   {
-            JSONArray compoundJsonArr = compoundStrings(dependencies, nmodChilds);
-            jsonGraphObj.put("TargetNode", compoundJsonArr);
-        }
-        else    {
-            Set<IndexedWord> dobjChilds = dependencies.getChildrenWithReln(rootVerb,
-                    UniversalEnglishGrammaticalRelations.OBJECT);
-
-            if (!dobjChilds.isEmpty())   {
-                JSONArray compoundJsonArr  = compoundStrings(dependencies, dobjChilds);
-                jsonGraphObj.put("TargetNode", compoundJsonArr );
-            }
-        }
-
-        return jsonGraphObj;
     }
 
     private void loadJSON(String filename) {
@@ -219,8 +82,9 @@ public class AnalyticsProcessor implements AutoCloseable {
                 JSONObject jsonObject = (JSONObject)i.next();
                 String news = (String) jsonObject.get("news");
                 Long newsId = (Long) jsonObject.get("newsId");
+                String collectionDate = (String) jsonObject.get("converted_collection_date");
 
-                this.newsArticles.add(new NewsArticles(news, newsId));
+                this.newsArticles.add(new NewsArticles(news, newsId, collectionDate));
             }
 
         } catch (Exception ex) {
@@ -228,7 +92,8 @@ public class AnalyticsProcessor implements AutoCloseable {
         }
    }
 
-    private void neo4JSemanticGraphBuilder(SemanticGraph dependencies, String docId, int senId) throws IOException {
+    private void neo4JSemanticGraphBuilder(SemanticGraph dependencies, String docId, int senId,
+                                           String collectionDate, String collectionTime) throws IOException {
         List<SemanticGraphEdge> edgeList = dependencies.edgeListSorted();
         ListIterator<SemanticGraphEdge> it = edgeList.listIterator();
 
@@ -236,35 +101,44 @@ public class AnalyticsProcessor implements AutoCloseable {
             SemanticGraphEdge edge = it.next();
             String source = edge.getSource().value();
             int sourceIdx = edge.getSource().index();
+
             String target = edge.getTarget().value();
             int targetIdx = edge.getTarget().index();
+
             String s_ner  = edge.getSource().ner();
             String t_ner  = edge.getTarget().ner();
+
             String s_tag  = edge.getSource().tag();
             String t_tag  = edge.getTarget().tag();
+
             String relation = edge.getRelation().toString();
             relation = relation.replace(":", "_");
 
             try (Session session = neo4jDriver.session()) {
-                session.run("MERGE (a:"+s_ner+" { word : \"" + source
+                session.run(
+                        "MERGE (a:"+s_ner+" { word : \"" + source
                         + "\", idx: "+ String.valueOf(sourceIdx)
                         + ", docId : "+ docId
-                        + ",senId : " + String.valueOf(senId)
-                        +", tag : \""+s_tag+"\" }) " +
-                        "MERGE (b:"+t_ner+" { word: \"" + target
+                        + ", senId : " + String.valueOf(senId)
+                        + ", Date: \"" + collectionDate + "\""
+                        + ", Time: \"" + collectionTime + "\""
+                        +", tag : \""+s_tag+"\" }) "
+                        + "MERGE (b:"+t_ner+" { word: \"" + target
                         + "\", idx: "+ String.valueOf(targetIdx)
                         + ", docId : "+ docId
                         + ",senId : " + String.valueOf(senId)
-                        + ", tag :\"" +t_tag+ "\"})" +
-                        "MERGE (a)-[r:dep{type:\""+relation+"\"}]->(b)");
+                        + ", Date: \"" + collectionDate + "\""
+                        + ", Time: \"" + collectionTime  + "\""
+                        + ", tag :\"" +t_tag+ "\"})"
+                        + "MERGE (a)-[r:dep{type:\""+relation+"\"}]->(b)");
             }
             catch (Exception e){
                 e.printStackTrace();
             }
-
         }
-        GraphReducer gr = new GraphReducer(neo4jDriver);
-        gr.runRules();
+
+//        GraphReducer gr = new GraphReducer(neo4jDriver);
+//        gr.runRules();
 
     }
 
@@ -298,8 +172,11 @@ public class AnalyticsProcessor implements AutoCloseable {
         for (NewsArticles newsArticle: newsArticles) {
             String text = newsArticle.newsArticle;
             String docID = newsArticle.newsID;
+            String collectionDate = newsArticle.collectionDate.split(" ")[0];
+            String collectionTime = newsArticle.collectionDate.split(" ")[1];
 
             text = replaceIllegalChars(text);
+
             Annotation document = new Annotation(text);
             pipeline.annotate(document);
 
@@ -309,48 +186,18 @@ public class AnalyticsProcessor implements AutoCloseable {
 
             int sentenceID = 0;
             for (CoreMap sentence : sentences) {
-                JSONObject jsonGraphObj;
                 sentenceID++;
 
                 // this is the Stanford dependency graph of the current sentence
                 SemanticGraph dependencies = sentence.get(SemanticGraphCoreAnnotations.EnhancedPlusPlusDependenciesAnnotation.class);
-                //            System.out.println(dependencies.toString());
-//                jsonGraphObj = buildDependency(dependencies);
 
-                JSONObject jsonIDobj = new JSONObject();
-                jsonIDobj.put("DocumentID", docID);
-                jsonIDobj.put("SentenceID", sentenceID);
-
-//                jsonGraphObj.put("ID",jsonIDobj);
-
-
-//                jsonGraphArray.add(jsonGraphObj);
-
-                neo4JSemanticGraphBuilder(dependencies, docID, sentenceID);
+                neo4JSemanticGraphBuilder(dependencies, docID, sentenceID,collectionDate,collectionTime);
 //                break;
             }
 
 //            break;
         }
-        // This is the coreference link graph
-        // Each chain stores a set of mentions that link to each other,
-        // along with a method for getting the most representative mention
-        // Both sentence and token offsets start at 1!
-//        Map<Integer, CorefChain> graph =
-//                document.get(CorefCoreAnnotations.CorefChainAnnotation.class);
-//
-//        System.out.println(graph.toString());
 
-//        try {
-//
-//            FileWriter file = new FileWriter(outFileName);
-//            file.write(jsonGraphArray.toJSONString());
-//            file.flush();
-//            file.close();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         System.out.println("All done!!!");
     }
 
